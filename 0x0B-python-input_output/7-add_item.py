259e561cb921867cff73dad12037bc28cb6badbf
davidmunene <davidmunene.dm@gmail.com>
2022-09-27 10:51:17 +0000

    Python - Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""adds all arguments to a python list and
-   saves them in a file
-"""
+"""add item"""
+
+
+import json
 import sys
 import os.path
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-
-    filename = "add_item.json"
-    if os.path.isfile(filename):
-        my_obj = load_from_json_file(filename)
-    else:
-        my_obj = []
-    my_obj.extend(sys.argv[1:])
-    save_to_json_file(my_obj, filename)
+filename = "add_item.json"
+if os.path.isfile(filename):
+    obj = load_from_json_file(filename)
+else:
+    obj = []
+obj.extend(sys.argv[1:])
+save_to_json_file(obj, filename)
